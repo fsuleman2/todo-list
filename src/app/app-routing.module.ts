df6e4d69880bd1f286a402253a32cc8eecdc5c23
fsuleman2 <sydsulmn7@gmail.com>
2021-11-30 13:16:45 +0530

    routing module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './MyComponents/about/about.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
 
 const routes: Routes = [
   {
-    path: 'about',
-    component: AboutComponent,
+    path: '',
+    component: TodosComponent,
     pathMatch: 'full',
-  }
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
 ];
 
 @NgModule({
